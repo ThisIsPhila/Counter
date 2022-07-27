d96f11dca43caaab05b4f34ae4b82d87a43b9a35
Phila Mavikane <68607571+ThisIsPhila@users.noreply.github.com>
2022-07-27 22:05:27 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// Set initial count
+let count = 0;
+
+// Select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+//Actions for Buttons
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const styles = e.target.classList;
+        
+        //Increment or decrement based on button
+        if (styles.contains('decrease')) {
+            count--;
+        } 
+        else if(styles.contains('increase')) {
+            count++;
+        }
+        else if(styles.contains('reset')) {
+            count = 0;
+        }
+
+        // Change color of the value
+        if(count < 0) {
+            value.style.color = 'red';
+        }
+        if(count > 0) {
+            value.style.color = 'green';
+        }
+        if(count === 0) {
+            value.style.color = 'black';
+        }
+        value.textContent = count;
+    });
+});
